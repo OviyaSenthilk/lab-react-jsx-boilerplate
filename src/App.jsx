98cd7d9d44c788819ec86cb805ab1a0bd55f0535
OviyaSenthilk <pji.oviya.s@kalvium.community>
2024-06-06 19:06:01 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,17 @@ const imageData = ()=>{
 }
 
 function App() {
-  // code here
+  const images = imageData(); 
+
+  return (
+    <div>
+      <h1>Kalvium Gallery</h1>
+      <img key={images[0].id} src={images[0].img} alt="" />
+      <img key={images[1].id} src={images[1].img} alt="" />
+      <img key={images[2].id} src={images[2].img} alt="" />
+      <img key={images[3].id} src={images[3].img} alt="" />
+    </div>
+  );git
 }
 
 export default App;
